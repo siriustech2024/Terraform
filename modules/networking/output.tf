aeb0b003da7cd1b36af99f10a5b35587365a3822
Denis Abreu <contato@siriustech.cloud>
2025-06-24 15:26:17 -0300

    Defining outputs

---
+++ modules/networking/output.tf
@@ -0,0 +1,15 @@
+output "subnet-pub-1a" {
+  value = aws_subnet.public_1a.id
+}
+
+output "subnet-pub-1b" {
+  value = aws_subnet.public_1b.id
+}
+
+output "subnet-priv-1a" {
+  value = aws_subnet.private_1a.id
+}
+
+output "subnet-priv-1b" {
+  value = aws_subnet.private_1b.id
+}
---
+++ output.tf
@@ -0,0 +1,15 @@
+output "subnet_pub_1a" {
+  value = module.eks_networking.subnet-pub-1a
+}
+
+output "subnet_pub_1b" {
+  value = module.eks_networking.subnet-pub-1b
+}
+
+output "subnet_priv_1a" {
+  value = module.eks_networking.subnet-priv-1a
+}
+
+output "subnet_priv_1b" {
+  value = module.eks_networking.subnet-priv-1b
+}
