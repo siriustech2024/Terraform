0bc949ca9ce9e94e2987d579798e0ead3c0bb2b5
Denis Abreu <contato@siriustech.cloud>
2025-06-27 15:54:40 -0300

    Security group inbound rule added

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -1,3 +1,7 @@
 output "eks_vpc_config" {
+
+  # A chave abaixo é composta pelo tipo_do_recurso.nome_do_recurso.argumento_ou_propriedade
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster#vpc_config-arguments
+
   value = aws_eks_cluster.eks_cluster.vpc_config
 }
